c565e6a0e6c0e3938563ef3584db0271064f6e48
just-silvia <barbagallo.silvia02@gmail.com>
2025-03-06 15:39:01 +0100

    made for loop to sum numbers in array

--- javascript/script.js
+++ javascript/script.js
@@ -69,4 +69,8 @@ console.log(value);
 // };
 
 const numbers = [1,2,3,4,5,6,7];
-console.log(numbers);
+let sum = 0;    
+for(let i = 0; i < numbers.length; i++){
+    sum += numbers[i];
+};
+console.log(sum);
