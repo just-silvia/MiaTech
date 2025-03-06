d11fff7052e7ddd1a58f9030321d8cb36ff29846
just-silvia <barbagallo.silvia02@gmail.com>
2025-03-06 15:46:56 +0100

    made for loop to read the array numbers in opposite

--- javascript/script.js
+++ javascript/script.js
@@ -69,8 +69,6 @@ console.log(value);
 // };
 
 const numbers = [1,2,3,4,5,6,7];
-let sum = 0;    
-for(let i = 0; i < numbers.length; i++){
-    sum += numbers[i];
+for(let i = numbers.length; i >= 0; i--){
+    console.log(numbers[i]);
 };
-console.log(sum);
