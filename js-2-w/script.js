4a79736b4b51cb7674a8e67096aaeb6688cf4bb6
just-silvia <barbagallo.silvia02@gmail.com>
2025-03-12 11:45:26 +0100

    added aggiungikm method and mostraChilometraggio

--- js-2-w/script.js
+++ js-2-w/script.js
@@ -1,4 +1,4 @@
-class automobile {
+/*class automobile {
     constructor(marca, modello, anno){
         this.marca = marca;
         this.modello = modello;
@@ -9,4 +9,27 @@ class automobile {
     }
 };
 const myCar = new automobile("Volkswagen", "Polo", "2019");
-console.log(myCar.descrizione());
+console.log(myCar.descrizione());*/
+
+// SECONDO ESERCIZIO
+
+class Automobile {
+    constructor(marca, modello, anno, chilometraggio){
+        this.marca = marca;
+        this.modello = modello;
+        this.anno = anno;
+        this.chilometraggio = chilometraggio;
+    }
+    aggiungiChilometri(km){
+        this.chilometraggio += km;
+    }
+    mostraChilometraggio(){
+        return `Il chilometraggio è: ${this.chilometraggio}`
+    }
+    descrizione(){
+        return `${this.marca} ${this.modello} ${this.anno}`;
+    }
+};
+const myCar = new Automobile("Volkswagen", "Polo", "2019", 20000);
+myCar.aggiungiChilometri(1000);
+console.log(myCar.descrizione(), myCar.mostraChilometraggio());
