c745a2dd7f0112d95b921770d0aab94face15cae
just-silvia <barbagallo.silvia02@gmail.com>
2025-03-13 16:43:14 +0100

    created protected method controllaChilometri and used it in subclass

--- js-2-w/script.js
+++ js-2-w/script.js
@@ -75,7 +75,7 @@ console.log(myCar.saluta(), myCar.descrizione());*/
 
 //QUINTO ESERCIZIO
 
-class Automobile {
+/*class Automobile {
     constructor(marca, modello, anno, chilometraggio){
         this.marca = marca;
         this.modello = modello;
@@ -101,5 +101,68 @@ class Automobile {
 };
 const myCar = new Automobile("Volkswagen", "Polo", "2019", 20000);
 /*myCar.aggiungiChilometri(1000);
-console.log(myCar.descrizione(), myCar.mostraChilometraggio());*/
+console.log(myCar.descrizione(), myCar.mostraChilometraggio());
+console.log(myCar.mostraEtà());*/
+
+//SESTO ESERCIZIO
+
+class Automobile {
+    constructor(marca, modello, anno, chilometraggio){
+        this.marca = marca;
+        this.modello = modello;
+        this.anno = anno;
+        this.chilometraggio = chilometraggio;
+    }
+    #calcolaEtà(){
+        const yearNow = new Date().getFullYear();
+        return yearNow - this.anno;
+    }
+    mostraEtà(){
+        return this.#calcolaEtà();
+    }
+    aggiungiChilometri(km){
+        this.chilometraggio += km;
+    }
+    mostraChilometraggio(){
+        return `Il chilometraggio è: ${this.chilometraggio}`
+    }
+    _controllaChilometri(){
+        if(this.chilometraggio > 100000){
+            return `Attenzione: questa macchina ha già molti chilometri!`
+        }else{
+            return `I km di questa macchina sono nella norma.`
+        }
+    }
+    descrizione(){
+        return `${this.marca} ${this.modello} ${this.anno}`;
+    }
+};
+const myCar = new Automobile("Volkswagen", "Polo", 2019, 200000);
+myCar.aggiungiChilometri(1000);
+console.log(myCar.descrizione(), myCar.mostraChilometraggio());
 console.log(myCar.mostraEtà());
+console.log(myCar._controllaChilometri())
+
+class Elettrica extends Automobile{
+    constructor(marca, modello, anno, chilometraggio, autonomia){
+        super(marca, modello, anno, chilometraggio)
+        this.autonomia = autonomia;
+    }
+    aggiungiAutonomia(km){
+        this.autonomia += km;
+    }
+    mostraAutonomia(){
+        return `L'autonomia è di: ${this.autonomia} km.`
+    }
+    controllaChilometri(){
+        return super._controllaChilometri()
+    }
+    descrizione(){
+        return `${super.descrizione()}, ${this.autonomia} km`
+    }
+}
+const myElectricCar = new Elettrica ("Fiat", "500e", 2022, 15000, 500);
+myElectricCar.aggiungiChilometri(500);
+myElectricCar.aggiungiAutonomia(100);
+// console.log(myElectricCar.descrizione(), myElectricCar.mostraChilometraggio(), myElectricCar.mostraAutonomia());
+console.log(myElectricCar.controllaChilometri());
