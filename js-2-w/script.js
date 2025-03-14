eb9b82cf7ed02d22ffa7f6f34dfcb756ef9144d7
just-silvia <barbagallo.silvia02@gmail.com>
2025-03-14 21:10:21 +0100

    created setter

--- js-2-w/script.js
+++ js-2-w/script.js
@@ -332,7 +332,7 @@ console.log(myCar.mostraContatoreChiamate());*/
 
 //UNDICESIMO ESERCIZIO
 
-class Automobile {
+/*class Automobile {
     constructor(marca, modello, chilometraggio){
         this.marca = marca;
         this.modello = modello;
@@ -377,4 +377,64 @@ class Automobile {
 
 const myCar = new Automobile("Fiat", "500", 20000);
 myCar.aggiungiChilometri(100);
-console.log(myCar.chilometraggio);  
+console.log(myCar.chilometraggio);*/
+
+//DODICESIMO ESERCIZIO
+
+class Automobile {
+    constructor(marca, modello, chilometraggio){
+        this.marca = marca;
+        this.modello = modello;
+        this._chilometraggio = chilometraggio;
+    }
+    #contatoreChiamate = 0;
+    #incrementaContatore(){
+        this.#contatoreChiamate++;
+    }
+    aggiungiChilometri(km){
+        this.#incrementaContatore();
+    }
+    mostraContatoreChiamate(){
+        return `Il metodo aggiungiChilometri è stato chiamato ${this.#contatoreChiamate} volte.`
+    }
+    mostraChilometraggio(){
+        return `Il chilometraggio è: ${this.chilometraggio}`
+    }
+    _controllaChilometri(){
+        if(this.chilometraggio > 100000){
+            return `Attenzione: questa macchina ha già molti chilometri!`
+        }else{
+            return "I km di questa macchina sono nella norma."
+        }
+    }
+    get chilometraggio(){
+        return this._chilometraggio;
+    }
+    set chilometraggio(valore){
+        if (valore >= this._chilometraggio){
+            this._chilometraggio = valore;
+        } else{
+            console.error("Non puoi ridurre il chilometraggio.");
+        }
+    }
+    static confrontaChilometri(auto1, auto2){
+        if(auto1.chilometraggio > auto2.chilometraggio){
+            return `${auto1.descrizione()} ha più chilometri di ${auto2.descrizione()}.`
+        }else if(auto1.chilometraggio < auto2.chilometraggio){
+            return `${auto2.descrizione()} ha più chilometri di ${auto1.descrizione()}.`
+        }else{
+            return `${auto1.descrizione()} e ${auto2.descrizione()} hanno lo stesso chilometraggio.`
+        }
+    }
+    descrizione(){
+    return `${this.marca} ${this.modello} ${this.chilometraggio}`;
+    }
+};
+
+const myCar = new Automobile("Fiat", "500", 12000);
+myCar.aggiungiChilometri(100);
+console.log(myCar.chilometraggio);
+myCar.chilometraggio = 900;
+console.log(myCar.chilometraggio);
+myCar.chilometraggio = 20500;
+console.log(myCar.chilometraggio); 
