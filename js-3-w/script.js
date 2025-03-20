514355fed10ced3693baed21d6371fb2a8cce546
just-silvia <barbagallo.silvia02@gmail.com>
2025-03-20 16:11:16 +0100

    made 5 default values

--- js-3-w/script.js
+++ js-3-w/script.js
@@ -3,6 +3,10 @@
 let [firstNumber, secondNumber] = numbers;
 console.log(firstNumber, secondNumber);*/
 // 2
-let numbers = [1, 2, 3, 4, 5];
+/*let numbers = [1, 2, 3, 4, 5];
 let [firstNumber, , thirdNumber] = numbers;
-console.log(firstNumber, thirdNumber);
+console.log(firstNumber, thirdNumber);*/
+// 3
+let numbers = [10,20];
+let [a = 10, b = 20, c = 30, d = 40, e = 50] = numbers;
+console.log(a, b, c, d, e);
