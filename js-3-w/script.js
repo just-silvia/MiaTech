07a3288778baba8440449c54e5649d90382fe608
just-silvia <barbagallo.silvia02@gmail.com>
2025-03-20 16:09:30 +0100

    destructuring but only taking first and third numbers

--- js-3-w/script.js
+++ js-3-w/script.js
@@ -1,4 +1,8 @@
 // 1
-/let numbers = [1, 2, 3, 4, 5];
+/*let numbers = [1, 2, 3, 4, 5];
 let [firstNumber, secondNumber] = numbers;
-console.log(firstNumber, secondNumber);
+console.log(firstNumber, secondNumber);*/
+// 2
+let numbers = [1, 2, 3, 4, 5];
+let [firstNumber, , thirdNumber] = numbers;
+console.log(firstNumber, thirdNumber);
