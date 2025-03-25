516421168ffc1d8e8d99410e916e9d29e066263e
just-silvia <barbagallo.silvia02@gmail.com>
2025-03-25 17:06:09 +0100

    promise chain with conditions

--- js-4-w/promises.js
+++ js-4-w/promises.js
@@ -31,7 +31,7 @@ promise.then(
     onSuccess => console.log(onSuccess)
 );*/
 // 7 Catena di promesse semplici
-new Promise((onSuccess, onError) => {
+/*new Promise((onSuccess, onError) => {
     setTimeout(() => onSuccess(2), 1000);
 
 }).then((result) => {
@@ -44,4 +44,23 @@ new Promise((onSuccess, onError) => {
     
 }).then((result) => {
     console.log(result);
-});
+});*/
+// 8 Catena di promesse con condizioni
+let verificaNumero = (numero) => {
+    return new Promise((onSuccess) => {
+        setTimeout(() => {
+            onSuccess(numero);
+            console.log(numero);
+        }, 1000);
+    }).then((numero) => {
+        if(numero % 2 === 0){
+            console.log("pari");
+        }else{
+            console.log("dispari");
+        }
+    }).then(() => {
+        console.log("risultato");
+    });
+};
+verificaNumero(6);
+verificaNumero(7);
