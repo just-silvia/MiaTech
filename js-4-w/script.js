1407db141a808690cabbd869f4218da6d3f5004d
just-silvia <barbagallo.silvia02@gmail.com>
2025-03-25 15:10:11 +0100

    function with callback and param

--- js-4-w/script.js
+++ js-4-w/script.js
@@ -1,7 +1,29 @@
 // CALLBACKS
 // 1. Funzione di base con callback
-const somma = (a, b) => {
-    return a + b;
-}
-const risultato = somma(2,2);
-console.log(risultato);
+/*const somma = (a, b, callback) => {
+    callback(a + b);
+};
+somma(2, 2, (risultato) => {
+    console.log(risultato)
+});*/
+// 2. Funzione con callback e passaggio di parametri
+const operazioni = (a, b, operazione, callback) => {
+    if(operazione === "moltiplicazione"){
+        callback(a * b);
+    }
+    if(operazione === "addizione"){
+        callback(a + b);
+    }
+    if(operazione === "divisione"){
+        callback(a / b);
+    }
+};
+operazioni(2, 5, "moltiplicazione", (risultato) => {
+    console.log(risultato)
+});
+operazioni(2, 8, "addizione", (risultato) => {
+    console.log(risultato)
+});
+operazioni(20, 2, "divisione", (risultato) => {
+    console.log(risultato)
+});
