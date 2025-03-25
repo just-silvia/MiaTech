3a19575d30dcf941c6d8cb581d22c714c5d27306
just-silvia <barbagallo.silvia02@gmail.com>
2025-03-25 16:26:44 +0100

    Promise with finally

--- js-4-w/promises.js
+++ js-4-w/promises.js
@@ -12,11 +12,21 @@ promise.then(
     onError => console.error(onError)
 );*/
 // 5 Gestione di una promessa con catch
-let promise = new Promise((onSuccess, onError) => {
+/*let promise = new Promise((onSuccess, onError) => {
     setTimeout(() => {
         onError(new Error("errore"))
     }, 2000);
 });
 promise.catch(
     error => console.error(error.message)
+);*/
+// 6 Promessa con finally
+let promise = new Promise((onSuccess, onError) => {
+    onSuccess("Ciao!")
+});
+promise.finally(
+    console.log("promise finalizzata")
+);
+promise.then(
+    onSuccess => console.log(onSuccess)
 );
