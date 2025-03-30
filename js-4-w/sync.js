69e8cc16fc28e9afec99eeea8cb0a6a5f04bac11
just-silvia <barbagallo.silvia02@gmail.com>
2025-03-30 20:15:30 +0200

    async functions in series

--- js-4-w/sync.js
+++ js-4-w/sync.js
@@ -12,7 +12,7 @@ async function loadMessage(){
     console.log(message);
 };*/
 // 17 Gestione degli errori con try e catch
-async function tryOperation(success){
+/*async function tryOperation(success){
     return new Promise((resolve, reject) => {
         setTimeout(() => {
             if(success){
@@ -34,4 +34,26 @@ async function loadOperation(success){
 // operazione completata
 loadOperation(true);
 // operazione fallita
-loadOperation(false);
+loadOperation(false);*/
+// 18 Funzioni asincrone in serie
+async function firstOperation(){
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve("Prima operazione risolta");
+        }, 2000);
+    })
+};
+async function secondOperation(){
+    return new Promise((resolve, reject) => {
+        setTimeout(() => {
+            resolve("Seconda operazione risolta");
+        }, 5000);
+    })
+};
+async function loadOperations(){
+    let result1 = await firstOperation();
+    console.log(result1);
+    let result2 = await secondOperation();
+    console.log(result2);
+};
+loadOperations();
