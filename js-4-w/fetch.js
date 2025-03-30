00330685ec607013e79fcaf1eb0d2204bf348855
just-silvia <barbagallo.silvia02@gmail.com>
2025-03-30 20:47:50 +0200

    POST request

--- js-4-w/fetch.js
+++ js-4-w/fetch.js
@@ -1,8 +1,33 @@
 // 19 Eseguire una richiesta GET semplice
-fetch("https://dog.ceo/api/breeds/image/random")
-.then((response) => {
-    console.log(response);
-})
-.catch((error) => {
-    console.error(error);
-})
+/*function getRequest(){
+    fetch("https://dog.ceo/api/breeds/image/random")
+    .then((response) => {
+        console.log(response);
+    })
+    .catch((error) => {
+        console.error(error);
+    });
+};*/
+// 20 Eseguire una richiesta POST
+function postRequest() {
+    const url = "https://catfact.ninja/fact";
+    const data = {
+        fact: "cats can sleep up to 18 hours a day."
+    };
+    const options = {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(data)
+    };
+    fetch(url, options)
+        .then((response) => response.json())
+        .then((data) => {
+            console.log(data);
+        })
+        .catch((error) => {
+            console.error(error);
+        });
+};
+postRequest();
