d5284793ae9c6b487dd415e4c62aad6599dfe6d7
just-silvia <barbagallo.silvia02@gmail.com>
2025-04-04 15:40:26 +0200

    DOMS

---
+++ DOMS/script.js
@@ -0,0 +1,31 @@
+// 6 getElementById
+/*let $paragraph = document.getElementById("paragraph");
+// aggiungo eventListener
+$paragraph.addEventListener("click", () => {
+    console.log("Paragraph clicked!");
+});*/
+// 7 querySelector
+/*let $button = document.querySelector("#button");
+// aggiungo eventListener
+$button.addEventListener("click", () => {
+    console.log("Button clicked!");
+});
+// 8 Modifica il testo di un paragrafo
+let $paragraph = document.getElementById("paragraph");
+// aggiungo eventListener
+$paragraph.addEventListener("click", () => {
+    $paragraph.innerText = "cliccato!";
+});*/
+// 9 Cambia lo stile NON FUNZIONA CHIEDI A MANFREDI
+/*let $paragraph = document.getElementById("paragraph");
+// aggiungo un bottone nel paragrafo
+$paragraph.innerHTML += '<button id="myButton">Nuovo Bottone</button>';
+// funzione che aggiunge il bottone dopo aver cliccato
+function addButtonEventListener() {
+    let $myButton = document.getElementById("myButton");
+    // aggiungo l'evento al bottone
+    $myButton.addEventListener("click", () => {
+        console.log("Bottone aggiunto!");
+    });
+};*/
+// 10 
