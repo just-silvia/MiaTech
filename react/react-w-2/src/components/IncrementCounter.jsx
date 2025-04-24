da0596b74c7b695a4f8a0e29584a8af176a6ace5
just-silvia <barbagallo.silvia02@gmail.com>
2025-04-24 18:12:05 +0200

    created button for increment

--- react/react-w-2/src/components/IncrementCounter.jsx
+++ react/react-w-2/src/components/IncrementCounter.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+/*import { useState } from "react";
 
 const IncrementCounter = () => {
     const [count, setCount] = useState(0);
@@ -6,6 +6,28 @@ const IncrementCounter = () => {
     return (
         <>
             <div>Count: {count}</div>
+        </>
+    );
+}
+
+export default IncrementCounter;*/
+
+// aggiungo funzione e bottone che incrementa di 1 il counter
+
+import { useState } from "react";
+
+const IncrementCounter = () => {
+    const [count, setCount] = useState(0);
+
+    const increment = () => {
+        setCount(count + 1);
+    };
+
+
+    return (
+        <>
+            <div>Count: {count}</div>
+            <button onClick={increment}>+</button>
         </>
     );
 }
