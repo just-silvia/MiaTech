8694aec8d12cd8bb3d139e445eca9f7790d9b4a1
just-silvia <barbagallo.silvia02@gmail.com>
2025-05-07 16:41:23 +0200

    Added Navbar with links to Home and About

--- react/react-w-4/src/App.jsx
+++ react/react-w-4/src/App.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
+import Navbar from "./components/Navbar";
 
 const App = () => {
   return(
     <>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
---
+++ react/react-w-4/src/components/Navbar.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+const Navbar = () => {
+    return(
+        <nav>
+            <div>
+            <Link to="/">Home</Link>
+            </div>
+            <div>
+                <Link to="/about">About</Link>
+            </div>
+        </nav>
+    );
+}
+
+export default Navbar;
