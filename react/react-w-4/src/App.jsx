882c292fd8f945fcbe102a0689afd850bd302569
just-silvia <barbagallo.silvia02@gmail.com>
2025-05-07 17:09:52 +0200

    Created Layout component and used Outlet to navigate through pages, incorporated also in App already

--- react/react-w-4/src/App.jsx
+++ react/react-w-4/src/App.jsx
@@ -3,14 +3,17 @@ import { Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Navbar from "./components/Navbar";
+import Layout from "./layouts/Layout";
 
 const App = () => {
   return(
     <>
       <Navbar />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
+        <Route path="/" element={<Layout />}>
+          <Route path="" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Route>
       </Routes>
     </>
   );
