50ce578bf0842df2a1e4fdeb716b4b260860e01b
just-silvia <barbagallo.silvia02@gmail.com>
2025-05-22 17:11:14 +0200

    made addTodo function

--- typescript/src/index.js
+++ typescript/src/index.js
@@ -1,1 +1,15 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var todos = [];
+var newId = 1;
+var addTodo = function (obj) {
+    var newTodo = {
+        id: newId++,
+        title: obj.title,
+        completed: false
+    };
+    todos.push(newTodo);
+    console.log(newTodo);
+    return newTodo;
+};
+addTodo({ title: "lista di todo" });
--- typescript/src/index.ts
+++ typescript/src/index.ts
@@ -1,3 +1,21 @@
 import { Todo } from "./types";
 
 const todos: Todo[] = [];
+
+let newId = 1;
+
+const addTodo = (obj: { title: string }): Todo => {
+    const newTodo: Todo = {
+        id: newId++,
+        title: obj.title,
+        completed: false
+    };
+
+    todos.push(newTodo);
+    console.log(newTodo);
+
+    return newTodo;
+}
+
+addTodo({ title: "lista di todo" });
+
