53b3c8718a8ea8226c15b670374b6bc8c5d960f9
just-silvia <barbagallo.silvia02@gmail.com>
2025-05-29 22:22:08 +0200

    added addTodo method to the class to add a todo in the class user

--- typescript/src/User.ts
+++ typescript/src/User.ts
@@ -1,11 +1,18 @@
+import { Todo } from "./types";
+
 export class User {
     id: number;
     name: string;
     email?: string;
+    todos: Todo[] = [];
 
     constructor(id: number, name: string, email?: string) {
         this.id = id;
         this.name = name;
         this.email = email;
+    }
+
+    addTodo(todo: Todo) {
+        this.todos.push(todo);
     }
 }
